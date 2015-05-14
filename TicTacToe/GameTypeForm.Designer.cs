f811f4392d87b3c0de114ef4e7c75ae79fb6ad14
Tyler <tyler_herrin@live.com>
2015-05-13 23:40:28 -0700

    Initial Commit

---
+++ TicTacToe/GameTypeForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TicTacToe
+{
+    partial class GameTypeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player = new System.Windows.Forms.Button();
+            this.computer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.player.Location = new System.Drawing.Point(12, 50);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(75, 23);
+            this.player.TabIndex = 0;
+            this.player.Text = "Player";
+            this.player.UseVisualStyleBackColor = true;
+            // 
+            // computer
+            // 
+            this.computer.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.computer.Location = new System.Drawing.Point(93, 50);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(75, 23);
+            this.computer.TabIndex = 1;
+            this.computer.Text = "Computer";
+            this.computer.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(267, 32);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Do you want to play against the computer or a person?";
+            // 
+            // cancel
+            // 
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(201, 50);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 3;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // GameTypeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 83);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(304, 122);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(304, 122);
+            this.Name = "GameTypeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Game Type";
+            this.Load += new System.EventHandler(this.GameTypeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button player;
+        private System.Windows.Forms.Button computer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button cancel;
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Boolean placeToken = false;
+        private int turn;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+           GameTypeForm gameTypeForm = new GameTypeForm();
+
+           DialogResult isSinglePlayer = gameTypeForm.ShowDialog();
+           if(isSinglePlayer == System.Windows.Forms.DialogResult.Yes)
+           {
+               SinglePlayer();
+           }
+           else if(isSinglePlayer == System.Windows.Forms.DialogResult.No)
+           {
+               TwoPlayer();
+           }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void SinglePlayer()
+        {
+            int playerFirst = displayTurnInfo();
+
+            
+            placeToken = true;
+            turn = 1;
+        }
+
+        private void TwoPlayer()
+        {
+            displayTurnInfo();
+
+            placeToken = true;
+            turn = 1;
+        }
+
+        private int displayTurnInfo()
+        {
+            Random rand = new Random();
+            int player1 = rand.Next(1,3);
+
+            if (player1 == 1)
+            {
+                Player1TextBox.Text = "Player 1 == X";
+                Player1TextBox.Visibility = Visibility.Visible;
+                Player2TextBox.Text = "Player 2 == Circle";
+                Player2TextBox.Visibility = Visibility.Visible;
+                PlayerTurnTextBox.Text = "Player 1's Turn";
+                PlayerTurnTextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Player1TextBox.Text = "Player 1 == Circle";
+                Player1TextBox.Visibility = Visibility.Visible;
+                Player2TextBox.Text = "Player 2 == X";
+                Player2TextBox.Visibility = Visibility.Visible;
+                PlayerTurnTextBox.Text = "Player 2's Turn";
+                PlayerTurnTextBox.Visibility = Visibility.Visible;
+            }
+
+            return player1;
+        }
+
+        private void Grid_Click(object sender, MouseButtonEventArgs e)
+        {
+            if(placeToken)
+            {
+                Point point = Mouse.GetPosition(gameGrid);
+                if (turn % 2 == 0)
+                    drawCircle(point);
+                else
+                    drawX(point);
+                turn++;
+                checkForWinner();
+            }
+            if (turn > 9)
+                placeToken = false;
+
+            
+        }
+
+        private void checkForWinner()
+        {
+            for(int row = 0; row < 3; row ++)
+            {
+                Shape shape1 = gameGrid.Children.Cast<Shape>().FirstOrDefault(e => Grid.GetColumn(e) == 0 && Grid.GetRow(e) == row);
+                Shape shape2 = gameGrid.Children.Cast<Shape>().FirstOrDefault(e => Grid.GetColumn(e) == 1 && Grid.GetRow(e) == row);
+                Shape shape3 = gameGrid.Children.Cast<Shape>().FirstOrDefault(e => Grid.GetColumn(e) == 2 && Grid.GetRow(e) == row);
+
+                if (shape1 != null && shape2 != null && shape3 != null)
+                {
+                    if (shape1.GetType() == shape2.GetType() && shape1.GetType() == shape3.GetType())
+                    {
+                        System.Windows.Forms.MessageBox.Show("Winner!");
+                        placeToken = false;
+                    } 
+                }
+            }
+        }
+
+        private void drawX(Point point)
+        {
+            Line line1 = new Line();
+            Line line2 = new Line();
+            line1.Stroke = System.Windows.Media.Brushes.Blue;
+            line2.Stroke = System.Windows.Media.Brushes.Blue;
+            line1.StrokeThickness = 5;
+            line2.StrokeThickness = 5;
+            
+            line1.X1 = 1;
+            line1.Y1 = 1;
+            line1.X2 = 100;
+            line1.Y2 = 100;
+            
+            line2.X1 = 1;
+            line2.Y1 = 100;
+            line2.X2 = 100;
+            line2.Y2 = 1;
+            
+            line1.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            line2.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            line1.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            line2.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            gameGrid.Children.Add(line1);
+            gameGrid.Children.Add(line2);
+
+            if (point.X < 128)
+            {
+                Grid.SetColumn(line1, 0);
+                Grid.SetColumn(line2, 0);
+            }
+            else if (point.X > 128 && point.X < 256)
+            {
+                Grid.SetColumn(line1, 1);
+                Grid.SetColumn(line2, 1);
+            }
+            else
+            {
+                Grid.SetColumn(line1, 2);
+                Grid.SetColumn(line2, 2);
+            }
+
+            if (point.Y < 128)
+            {
+                Grid.SetRow(line1, 0);
+                Grid.SetRow(line2, 0);
+            }
+            else if (point.Y > 128 && point.Y < 256)
+            {
+                Grid.SetRow(line1, 1);
+                Grid.SetRow(line2, 1);
+            }
+            else
+            {
+                Grid.SetRow(line1, 2);
+                Grid.SetRow(line2, 2);
+            }
+        }
+
+        private void drawCircle(Point point)
+        {
+            Ellipse circle = new Ellipse();
+            circle.Stroke = System.Windows.Media.Brushes.Red;
+            circle.Height = 100;
+            circle.Width = 100;
+            circle.StrokeThickness = 2;
+
+            circle.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            circle.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            gameGrid.Children.Add(circle);
+
+            if (point.X < 128)
+                Grid.SetColumn(circle, 0);
+            else if (point.X > 128 && point.X < 256)
+                Grid.SetColumn(circle, 1);
+            else
+                Grid.SetColumn(circle, 2);
+
+            if (point.Y < 128)
+                Grid.SetRow(circle, 0);
+            else if (point.Y > 128 && point.Y < 256)
+                Grid.SetRow(circle, 1);
+            else
+                Grid.SetRow(circle, 2);
+        }
+
+    }
+}
